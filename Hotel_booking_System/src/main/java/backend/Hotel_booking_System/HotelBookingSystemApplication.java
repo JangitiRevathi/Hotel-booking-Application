fda17f68ee0844bee041da783e56eccd2dccb0a6
Sailihish45 <sailithish45@gmail.com>
2026-04-16 11:58:31 +0530

    added initial commit

---
+++ Hotel_booking_System/src/main/java/backend/Hotel_booking_System/HotelBookingSystemApplication.java
@@ -0,0 +1,14 @@
+package backend.Hotel_booking_System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class
+HotelBookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelBookingSystemApplication.class, args);
+	}
+
+}
