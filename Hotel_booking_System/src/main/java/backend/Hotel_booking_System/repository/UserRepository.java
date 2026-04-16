92681eb639b06f1f48f16d93feab747fefdf6f6a
Vishnuu_84 <chinthalavishnuu@gmail.com>
2026-04-16 12:21:15 +0530

    created userRepo

--- Hotel_booking_System/src/main/java/backend/Hotel_booking_System/repository/UserRepository.java
+++ Hotel_booking_System/src/main/java/backend/Hotel_booking_System/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package backend.Hotel_booking_System.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import backend.Hotel_booking_System.service.User;
+public interface UserRepository extends JpaRepository<User, Long> {
+    User findByUsername(String username);
 }
